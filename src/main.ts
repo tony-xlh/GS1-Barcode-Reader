5f278358c3286af2cd9f6d90185b8456d7257585
xulihang <xulihanghai@163.com>
2023-08-14 17:45:26 +0800

    prepend application identifier for omnidirectional codes

--- src/main.ts
+++ src/main.ts
@@ -91,7 +91,7 @@ function parseGS1Barcode(result:TextResult){
   text = text.replace(/{GS}/g,"|");
   text = text.replace(//g,"|");
   let segments = text.split("|");
-  if (result.barcodeFormatString === "GS1 Composite Code") {
+  if (result.barcodeFormatString === "GS1 Composite Code" || result.barcodeFormatString.indexOf("Omnidirectional") != -1) {
     segments[0] = "01" + segments[0]; //add application identifier
   }
   console.log(segments);
